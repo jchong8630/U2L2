21fdc601982bc5f7b880ec182df294d08dbd1b0b
Justin Chong <jchong8630@bths.edu>
2022-09-23 20:05:14 -0400

    Finished Poem

--- src/Runner.java
+++ src/Runner.java
@@ -2,19 +2,8 @@ import java.util.Scanner;
 
 public class Runner {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        System.out.println("Boards Of Lumber: ");
-        int lumber = s.nextInt();
-        System.out.println("Windows: ");
-        int windows = s.nextInt();
-        System.out.println("Tax Rate: ");
-        double taxRate = s.nextDouble();
-
-        ConstructionPricer house1 = new ConstructionPricer(11.50, 25.75 , taxRate);
-        double cost = house1.materialsCost(lumber, windows);
-        System.out.println("Material Cost: " + cost);
-        double costWithTax = house1.totalWithTax(cost);
-        System.out.println("Total Cost: " + costWithTax);
+        Poem poem = new Poem("Adrian");
+        poem.printPoem();
 
     }
 }
